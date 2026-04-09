198aab0d31cc4491ad8adc663b47cbddd0ae73ae
Hemsuriya <hemsuriya1412@gmail.com>
2026-04-09 22:18:12 +0530

    Remove BOM from experience component

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,4 +1,4 @@
-﻿import { motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 const experiences = [
   {
@@ -70,4 +70,3 @@ function Experience() {
 }
 
 export default Experience;
-
